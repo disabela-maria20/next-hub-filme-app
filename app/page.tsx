27f67c3e3f374608bc83599b7c4afab39e07f992
Isabela Maria Ribeiro Da Silva <imsilva9@topazevolution.com>
2026-07-28 11:53:11 -0300

    feat: add icon

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,8 @@
 import Home from "@/views/home";
+import type { Metadata } from "next";
+import config from "@/data/config.json";
+export const metadata: Metadata = config.seo;
 
-export default function pageHome() {
+export default function PageHome() {
   return <Home />;
 }
