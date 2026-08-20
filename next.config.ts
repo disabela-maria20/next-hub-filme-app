ebd5f8e3accec740e1a72c023c83edf77bb35650
Isabela M <isabela10014@hotmail.com>
2026-08-20 17:00:28 -0300

    fix:

--- next.config.ts
+++ next.config.ts
@@ -4,8 +4,8 @@ const nextConfig: NextConfig = {
   output: "export",
   distDir: "build",
   trailingSlash: true,
-  // basePath: "/mashaeosursos",
-  // assetPrefix: "/mashaeosursos",
+  basePath: "/mashaeosursos",
+ assetPrefix: "/mashaeosursos",
 };
 
 export default nextConfig;
